d45cafb355f50cac181660f81944d66cb445f846
pom <pom@laputa.lan>
2021-11-27 13:31:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='playlang',
+    version='2.0',
+    description='playlang',
+    author='Pom',
+    author_email='pom@laputa.lan',
+    url='https://git.laputa.lan/pom/playlang',
+    packages=['playlang'],
+    python_requires=">=3.6",
+)
